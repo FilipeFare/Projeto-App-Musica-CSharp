a20f48669443f508dab39387ef231def2bb9391c
FilipeFare <124682536+FilipeFare@users.noreply.github.com>
2025-04-15 02:43:30 -0300

    Add files via upload

---
+++ ScreenSound03-projeto-inicial/ScreenSound/Menus/Menu.cs
@@ -0,0 +1,20 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class Menu
+{      public void ExibirTituloDaOpcao(string titulo)
+    {
+        int quantidadeDeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '=');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(titulo);
+        Console.WriteLine(asteriscos + "\n");
+    }
+    //Virtual permite Sobrescrever a função da base usando o override na herança
+    public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+
+    }
+}
